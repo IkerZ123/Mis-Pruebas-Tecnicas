065373b6b1df4b4be4af99a1cac3db5f35195849
IkerZ123 <ikerbenitopascual@gmail.com>
2024-12-01 22:00:19 +0100

    Añadir a la lista

--- pruebas-web/Lista-Lectura-1/src/App.tsx
+++ pruebas-web/Lista-Lectura-1/src/App.tsx
@@ -5,33 +5,82 @@ import './App.css'
 
 import books from './books.json' assert { type: 'json' }
 
+interface Book{
+  title: string;
+  pages: number;
+  genre: string;
+  cover: string;
+  synopsis: string;
+  year: number;
+  ISBN: string
+  author: Author;
+}
+
+interface Author {
+  name: string;
+  otherBooks: string[];
+}
+
 function App() {
+
+  // Nuestra Lista de lectura: guardamos los ISBN como identicadores
+  // Importante hacerlo con array de Book!
+  const [readings, setReadings] = useState<Book[]>([])
 
   // Coger todos los datos de todos los libros
   const lib = JSON.parse(JSON.stringify(books))
 
   // Mostrar los libros en la lista principal
   const showBooksAvailable = () => {
-    return lib.library.map((item: { book: { title: string, cover: string }}) => {
-      const book = item.book
-      return <div>
-        <h2>{book.title}</h2>
-        <img src={book.cover} alt="Titulo del libro"></img>
-        <button onClick={addBookToRead}>Añadir a la Lista de Lectura</button>
+    return (
+      <div className="image-container">
+        {lib.library.map((item: { book: Book }) => {
+          const book = item.book;
+          return (
+            <div key={book.ISBN}>
+              <img 
+                src={book.cover} 
+                alt={book.title}
+              />
+              <button onClick={() => addBookToRead(book)}>Añadir a la lista de Lectura</button>
+            </div>
+          )
+        })}
       </div>
-    })
+    )
   }
 
-  const addBookToRead = () => {
-
+  // Añadir el libro que hemos seleccionado a nuestra lista de lectura
+  // Lo metemos como objeto y toda su informacion por si la necesitaramos en algun momento
+  const addBookToRead = (book: Book) => {
+    if(!readings.some(item => item.ISBN === book.ISBN)){
+      setReadings(previous => [...previous, {
+        title: book.title,
+        pages: book.pages,
+        genre: book.genre,
+        cover: book.cover,
+        synopsis: book.synopsis,
+        year: book.year,
+        ISBN: book.ISBN,
+        author: book.author}])
+    }
   }
+
+  
 
   return (
     <>
      <main>
       <h1>Primera Prueba Técnica: Lista de Lectura</h1>
-      {showBooksAvailable()}
-     </main>
+      <div className='ListsContainer'>
+        <div>
+          {showBooksAvailable()}
+        </div>
+        <div>
+          {showReadings()}
+        </div>
+      </div>
+      </main>
     </>
   )
 }

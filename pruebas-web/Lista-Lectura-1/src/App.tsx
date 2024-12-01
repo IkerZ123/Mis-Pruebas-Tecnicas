4bd9eb872dd5cbb99d9d76b17162416ce81e6efb
IkerZ123 <ikerbenitopascual@gmail.com>
2024-12-01 20:59:46 +0100

    Mostrar libros disponibles

--- pruebas-web/Lista-Lectura-1/src/App.tsx
+++ pruebas-web/Lista-Lectura-1/src/App.tsx
@@ -3,12 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import books from './books.json' assert { type: 'json' }
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Coger todos los datos de todos los libros
+  const lib = JSON.parse(JSON.stringify(books))
+
+  // Mostrar los libros en la lista principal
+  const showBooksAvailable = () => {
+    return lib.library.map((item: { book: { title: string, cover: string }}) => {
+      const book = item.book
+      return <div>
+        <h2>{book.title}</h2>
+        <img src={book.cover} alt="Titulo del libro"></img>
+        <button onClick={addBookToRead}>Añadir a la Lista de Lectura</button>
+      </div>
+    })
+  }
+
+  const addBookToRead = () => {
+
+  }
 
   return (
     <>
-     
+     <main>
+      <h1>Primera Prueba Técnica: Lista de Lectura</h1>
+      {showBooksAvailable()}
+     </main>
     </>
   )
 }
